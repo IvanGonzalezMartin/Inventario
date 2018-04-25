d38fd33e0b7b363d4c357e90bbd5502cd03c5500
victor-afiz <afeezhammed@gmail.com>
2018-04-25 10:05:56 +0200

    Merge con la master

--- src/Domain/Entity/GestorEntity/GestorEntity.php
+++ src/Domain/Entity/GestorEntity/GestorEntity.php
@@ -32,4 +32,136 @@ class GestorEntity
      */
     private $role;
 
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean" ,options={"default":true})
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="string" ,length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string" ,length=255)
+     */
+    private $adminPhoto;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param mixed $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdminPhoto()
+    {
+        return $this->adminPhoto;
+    }
+
+    /**
+     * @param mixed $adminPhoto
+     */
+    public function setAdminPhoto($adminPhoto)
+    {
+        $this->adminPhoto = $adminPhoto;
+    }
+
 }
---
+++ src/Infrastructure/Entity/OrderRepository/OrderDoctrineRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: programador
+ * Date: 25/04/18
+ * Time: 10:04
+ */
+
+namespace App\Infrastructure\Entity\OrderRepository;
+
+
+use App\Domain\Entity\OrderEntity\OrderEntityRepository;
+
+class OrderDoctrineRepository implements OrderEntityRepository
+{
+
+}
