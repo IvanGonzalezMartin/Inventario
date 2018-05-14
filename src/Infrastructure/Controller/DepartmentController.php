0220dd73febbb5753c383aaee902981ce80cb402
victor-afiz <afeezhammed@gmail.com>
2018-05-14 11:33:09 +0200

    14-mayo-2018 11:33

--- src/Infrastructure/Controller/DepartmentController.php
+++ src/Infrastructure/Controller/DepartmentController.php
@@ -10,6 +10,8 @@ namespace App\Infrastructure\Controller;
 
 use App\Application\Department\Create\DepartmentCreate;
 use App\Application\Department\Create\DepartmentCreateCommand;
+use App\Application\Department\Update\DepartmentUpdate;
+use App\Application\Department\Update\DepartmentUpdateCommand;
 use App\Infrastructure\Utils\MyOwnHttpCodes;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,10 +19,12 @@ use Symfony\Component\HttpFoundation\Request;
 class DepartmentController
 {
     private $departmentCreate;
+    private $departmentUpdate;
 
-    public function __construct(DepartmentCreate $departmentCreate)
+    public function __construct(DepartmentCreate $departmentCreate, DepartmentUpdate $departmentUpdate)
     {
         $this->departmentCreate = $departmentCreate;
+        $this->departmentUpdate = $departmentUpdate;
     }
 
     public function createDepartment(Request $request)
@@ -29,6 +33,16 @@ class DepartmentController
         $name = $request->query->get('name');
         $departmentCreateCommand = new DepartmentCreateCommand($parentID , $name);
         $this->departmentCreate->handler($departmentCreateCommand);
+
+        return new JsonResponse([],MyOwnHttpCodes::HTTP_OK);
+    }
+
+    public function updateDepartment(Request $request)
+    {
+        $id = $request->query->get('id');
+        $name = $request->query->get('name');
+        $departmentUpdateCommand = new DepartmentUpdateCommand($id , $name);
+        $this->departmentUpdate->handler($departmentUpdateCommand);
 
         return new JsonResponse([],MyOwnHttpCodes::HTTP_OK);
     }
